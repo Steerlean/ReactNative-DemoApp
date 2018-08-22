802b7783a356b1806dd49e100ab2bd56ab0a4e56
Shraddha Agarwal <shraddha.agarwal726@gmail.com>
2018-08-22 12:46:36 +0530

    Added android geo location key in test and release properties file

--- Release_Properties.js
+++ Release_Properties.js
@@ -2,5 +2,4 @@ export const Web_CLient_ID = "1060192372477-8q378g6lc2ua4382uuli623qj51q3105.app
 export const API_key= "AIzaSyC1XLzcGsad9ji7aMNSdf5-9yliWeHinJQ";
 export const spreadsheet_ID = "1fX-JTVl4V3l9bl30qL2wE-TJ-mI9wjxD1_gUUYJ9I1g";
 export const sheet_ID_GID="1196448766";
-
-
+export const android_geo_API_KEY="AIzaSyDRplsfZcxrl25wY5E95NQm1mVXjVMe874";
