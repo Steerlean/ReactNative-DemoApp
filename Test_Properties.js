35dc7cfa49b05c057e3c4cf6bfe29a155fb2dbab
Aashish Gaikwad <aashish.gaikwad@steerlean.com>
2019-09-18 15:10:22 +0530

    Removed Properties file.

--- Test_Properties.js
+++
@@ -1,5 +0,0 @@
-export const Web_CLient_ID = "5593423861-hg1s7arlubu7u75t7ssjqb3oi0rqj1le.apps.googleusercontent.com";
-export const API_key = "AIzaSyBnHeKJ6kNV4EQWEIojB2jFjzrqOKYbtSA";
-export const spreadsheet_ID = "1_sIKjoYU7wDlGysnna9cXvTLQdGGjjmP3lFzMmj0aWU";
-export const sheet_ID_GID="396777180";
-export const android_geo_API_KEY="AIzaSyBFLmw4uk91xwCv0A5iPkl-ayQBeDUx9jM";
